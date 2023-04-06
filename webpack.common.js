92379bb21bc90338a024340ec4552c646eea9ff9
Valentin Eduardo Gonzalez Morales <valentin.eduardo.g94@gmail.com>
2023-04-06 18:15:57 -0500

    first commit

---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path")
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, "src", "index.jsx"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/i,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
+}
